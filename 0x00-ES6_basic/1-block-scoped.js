309869690b40cab72a71e7b08fda88e825e3b544
surahj <odebodeopeyemi@gmail.com>
2022-12-20 00:50:24 +0100

    Fix error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,11 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    
+
     const task = true;
 
     const task2 = false;
+    console.log(task, task2);
   }
 
   return [task, task2];
