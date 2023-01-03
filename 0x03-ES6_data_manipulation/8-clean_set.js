7c3f7ad27e4660ff05294e039c7954c9526e8ff2
surahj <odebodeopeyemi@gmail.com>
2023-01-03 17:58:14 +0100

    Create 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+const cleanSet = (set, startString) => {
+  if (!startString) {
+    return '';
+  }
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+  return result.join('-');
+};
+
+export default cleanSet;
