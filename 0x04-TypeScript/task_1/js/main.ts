af98d3852c36e3b2cbf29e93ba11eac6c87ab697
surahj <odebodeopeyemi@gmail.com>
2023-01-05 21:15:22 +0100

    Update task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,48 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+};
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+};
+
+const printTeacher = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+class StudentClass implements StudentInterface {
+  public firstName: string;
+  public lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  public displayName(): string {
+    return `${this.firstName}`;
+  }
+}
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
