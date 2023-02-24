bd9d8ad8e4494aa26a173b59fcf82989d8db4e09
surahj <odebodeopeyemi@gmail.com>
2023-02-24 02:33:24 +0100

    Fix error

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
